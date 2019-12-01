46fd1c67f6fa6c877cb9a1022c99592903bf859f
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-01 01:32:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence1)
-  sentence1 = "Hello there, and how are you?" 
+  sentence1 = "Hello there, and how are you?"
   sentence1 do |sentences|
     puts "#{sentences}".reverse
   end
